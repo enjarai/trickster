736897411facf5de834542d80d827b39a49b347b
enjarai <enjarai123@gmail.com>
2024-07-03 23:17:24 +0200

    Support maps with shadowy shenanigans

---
+++ src/main/java/dev/enjarai/trickster/mixin/AbstractBlockStateMixin.java
@@ -0,0 +1,33 @@
+package dev.enjarai.trickster.mixin;
+
+import dev.enjarai.trickster.cca.ModChunkCumponents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.MapColor;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public class AbstractBlockStateMixin {
+    @Inject(
+            method = "getMapColor(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/MapColor;",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void disguiseBlockOnMap(BlockView view, BlockPos pos, CallbackInfoReturnable<MapColor> cir) {
+        if (view instanceof ServerWorld world) {
+            var chunk = world.getChunk(pos);
+            var component = ModChunkCumponents.SHADOW_DISGUISE_MAP.getNullable(chunk);
+            if (component != null) {
+                var disguise = component.getFunnyState(pos);
+                if (disguise != null) {
+                    cir.setReturnValue(disguise.getBlock().getDefaultMapColor());
+                }
+            }
+        }
+    }
+}
