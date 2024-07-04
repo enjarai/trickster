d5467eb09a9735071137324402dfb3b442dc6651
enjarai <enjarai123@gmail.com>
2024-07-04 23:03:47 +0200

    Performance fixes and solid block check, dont want flowers making us blind after all.

--- src/main/java/dev/enjarai/trickster/mixin/LivingEntityMixin.java
+++ src/main/java/dev/enjarai/trickster/mixin/LivingEntityMixin.java
@@ -17,34 +17,57 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Unique
+    private static final StatusEffectInstance PERM_BLINDNESS = new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 2);
+    @Unique
+    private boolean inShadowBlock;
+
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @ModifyReturnValue(method = "hasStatusEffect", at = @At("RETURN"))
+    @ModifyReturnValue(
+            method = "hasStatusEffect",
+            at = @At("RETURN")
+    )
     public boolean hasStatusEffect(boolean original, RegistryEntry<StatusEffect> effect) {
         if (!(((LivingEntity)(Object)this) instanceof PlayerEntity))
             return original;
 
-        if (effect == StatusEffects.BLINDNESS)
-            return original || inShadowBlock(this.getWorld(), BlockPos.ofFloored(this.getEyePos()));
+        if (effect == StatusEffects.BLINDNESS) {
+            return original || inShadowBlock;
+        }
 
         return original;
     }
 
     @Nullable
-    @ModifyReturnValue(method = "getStatusEffect", at = @At("RETURN"))
+    @ModifyReturnValue(
+            method = "getStatusEffect",
+            at = @At("RETURN")
+    )
     public StatusEffectInstance getStatusEffect(StatusEffectInstance original, RegistryEntry<StatusEffect> effect) {
         if (!(((LivingEntity)(Object)this) instanceof PlayerEntity))
             return original;
 
-        if (effect == StatusEffects.BLINDNESS && original == null && inShadowBlock(this.getWorld(), BlockPos.ofFloored(this.getEyePos())))
-            return new StatusEffectInstance(StatusEffects.BLINDNESS, 100, 2);
+        if (effect == StatusEffects.BLINDNESS && original == null && inShadowBlock) {
+            return PERM_BLINDNESS;
+        }
 
         return original;
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At("TAIL")
+    )
+    private void checkShadowBlockState(CallbackInfo ci) {
+        inShadowBlock = inShadowBlock(getWorld(), BlockPos.ofFloored(this.getEyePos()));
     }
 
     @Unique
@@ -57,6 +80,6 @@ public abstract class LivingEntityMixin extends Entity {
         var shadowBlocks = ModChunkCumponents.SHADOW_DISGUISE_MAP.get(chunk);
         var funnyState = shadowBlocks.getFunnyState(blockPos);
 
-        return funnyState != null;
+        return funnyState != null && funnyState.isSolidBlock(world, blockPos);
     }
 }
