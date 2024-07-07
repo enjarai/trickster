ea01348867b8f1d6872d70985aef736e17fcdb88
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-07-07 18:11:51 -0400

    Only tool items may have their spells triggered now

--- src/main/java/dev/enjarai/trickster/spell/ItemTriggerProvider.java
+++ src/main/java/dev/enjarai/trickster/spell/ItemTriggerProvider.java
@@ -2,19 +2,24 @@ package dev.enjarai.trickster.spell;
 
 import dev.enjarai.trickster.item.component.ModComponents;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ToolItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.Arrays;
 
 public interface ItemTriggerProvider {
     default void trickster$triggerMainHand(ServerPlayerEntity player, Fragment... arguments) {
-        var spellComponent = player.getMainHandStack().get(ModComponents.SPELL);
+        var stack = player.getMainHandStack();
 
-        if (spellComponent != null) {
-            var ctx = new PlayerSpellContext(player, EquipmentSlot.MAINHAND);
-            ctx.pushPartGlyph(Arrays.stream(arguments).toList());
-            spellComponent.spell().run(ctx);
-            ctx.popPartGlyph();
+        if (stack.getItem() instanceof ToolItem) {
+            var spellComponent = stack.get(ModComponents.SPELL);
+
+            if (spellComponent != null) {
+                var ctx = new PlayerSpellContext(player, EquipmentSlot.MAINHAND);
+                ctx.pushPartGlyph(Arrays.stream(arguments).toList());
+                spellComponent.spell().run(ctx);
+                ctx.popPartGlyph();
+            }
         }
     }
 }
