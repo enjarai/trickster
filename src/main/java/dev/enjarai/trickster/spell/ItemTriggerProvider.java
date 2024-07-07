44da213ea7498edfc23efce297bc7be611324a03
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-07-07 19:23:56 -0400

    Pound the ground with no weapon found

--- src/main/java/dev/enjarai/trickster/spell/ItemTriggerProvider.java
+++ src/main/java/dev/enjarai/trickster/spell/ItemTriggerProvider.java
@@ -2,24 +2,38 @@ package dev.enjarai.trickster.spell;
 
 import dev.enjarai.trickster.item.component.ModComponents;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.util.Arrays;
+import java.util.List;
 
 public interface ItemTriggerProvider {
     default void trickster$triggerMainHand(ServerPlayerEntity player, Fragment... arguments) {
         var stack = player.getMainHandStack();
 
         if (stack.getItem() instanceof ToolItem) {
-            var spellComponent = stack.get(ModComponents.SPELL);
+            trickster$trigger(player, stack, List.of(arguments));
+        }
+    }
 
-            if (spellComponent != null) {
-                var ctx = new PlayerSpellContext(player, EquipmentSlot.MAINHAND);
-                ctx.pushPartGlyph(Arrays.stream(arguments).toList());
-                spellComponent.spell().run(ctx);
-                ctx.popPartGlyph();
-            }
+    default void trickster$triggerBoots(ServerPlayerEntity player, Fragment... arguments) {
+        var stack = player.getAllArmorItems().iterator().next();
+
+        if (stack.getItem() instanceof ArmorItem) {
+            trickster$trigger(player, stack, List.of(arguments));
+        }
+    }
+
+    default void trickster$trigger(ServerPlayerEntity player, ItemStack stack, List<Fragment> arguments) {
+        var spellComponent = stack.get(ModComponents.SPELL);
+
+        if (spellComponent != null) {
+            var ctx = new PlayerSpellContext(player, EquipmentSlot.MAINHAND);
+            ctx.pushPartGlyph(arguments);
+            spellComponent.spell().run(ctx);
+            ctx.popPartGlyph();
         }
     }
 }
