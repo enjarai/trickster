6357d566825dfd0ae50915939d1d9d8f38049d30
enjarai <enjarai123@gmail.com>
2024-08-10 23:25:43 +0200

    Inversion

--- src/main/java/dev/enjarai/trickster/spell/trick/vector/InvertTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/vector/InvertTrick.java
@@ -4,6 +4,7 @@ import dev.enjarai.trickster.spell.Fragment;
 import dev.enjarai.trickster.spell.Pattern;
 import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.fragment.FragmentType;
+import dev.enjarai.trickster.spell.fragment.NumberFragment;
 import dev.enjarai.trickster.spell.fragment.VectorFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.trick.blunder.BlunderException;
@@ -11,15 +12,19 @@ import org.joml.Vector3d;
 
 import java.util.List;
 
-public class ReverseVectorTrick extends Trick {
-    public ReverseVectorTrick() {
+public class InvertTrick extends Trick {
+    public InvertTrick() {
         super(Pattern.of(3, 4, 5, 2, 3));
     }
 
     @Override
     public Fragment activate(SpellContext ctx, List<Fragment> fragments) throws BlunderException {
-        var vec = expectInput(fragments, FragmentType.VECTOR, 0);
+        var vec = supposeInput(fragments, FragmentType.VECTOR, 0);
+        if (vec.isPresent()) {
+            return new VectorFragment(vec.get().vector().mul(-1, new Vector3d()));
+        }
 
-        return new VectorFragment(vec.vector().mul(-1, new Vector3d()));
+        var number = expectInput(fragments, FragmentType.NUMBER, 0);
+        return new NumberFragment(-number.number());
     }
 }
