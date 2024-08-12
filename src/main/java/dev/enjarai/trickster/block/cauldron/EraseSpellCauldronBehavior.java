ca11e61f4463f60d9c82294b4bc355cc4b855a46
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-08-12 08:07:38 -0400

    They should stack now

--- src/main/java/dev/enjarai/trickster/block/cauldron/EraseSpellCauldronBehavior.java
+++ src/main/java/dev/enjarai/trickster/block/cauldron/EraseSpellCauldronBehavior.java
@@ -2,6 +2,8 @@ package dev.enjarai.trickster.block.cauldron;
 
 import dev.enjarai.trickster.item.ModItems;
 import dev.enjarai.trickster.item.component.ModComponents;
+import dev.enjarai.trickster.item.component.SpellComponent;
+import dev.enjarai.trickster.spell.SpellPart;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
@@ -16,7 +18,7 @@ public class EraseSpellCauldronBehavior implements CauldronBehavior {
     @Override
     public ItemActionResult interact(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack stack) {
         stack.remove(ModComponents.WRITTEN_SCROLL_META);
-        stack.remove(ModComponents.SPELL);
+        stack.set(ModComponents.SPELL, new SpellComponent(new SpellPart()));
         player.setStackInHand(hand, stack.withItem(ModItems.SCROLL_AND_QUILL));
         LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
         return ItemActionResult.SUCCESS;
