30f266a2832decf800facf4485e6c68f8df71e05
enjarai <enjarai123@gmail.com>
2024-08-16 15:16:13 +0200

    Hehe, hypoflux

--- src/main/java/dev/enjarai/trickster/cca/ManaComponent.java
+++ src/main/java/dev/enjarai/trickster/cca/ManaComponent.java
@@ -1,12 +1,17 @@
 package dev.enjarai.trickster.cca;
 
 import dev.enjarai.trickster.ModAttachments;
+import dev.enjarai.trickster.effects.ModEffects;
 import dev.enjarai.trickster.entity.ModEntities;
+import dev.enjarai.trickster.item.component.ModComponents;
 import dev.enjarai.trickster.misc.ModDamageTypes;
+import dev.enjarai.trickster.spell.execution.executor.DefaultSpellExecutor;
 import dev.enjarai.trickster.spell.mana.ManaPool;
 import dev.enjarai.trickster.spell.mana.ManaPoolType;
 import dev.enjarai.trickster.spell.mana.SimpleManaPool;
+import io.wispforest.accessories.api.slot.SlotReference;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.registry.RegistryWrapper;
@@ -49,8 +54,58 @@ public class ManaComponent extends SimpleManaPool implements AutoSyncedComponent
         if (manaDevoid)
             return;
 
+        if (entity.age % 80 == 0 && entity instanceof ServerPlayerEntity player) {
+            processManaDeficiency(player);
+        }
+
         maxMana = ManaPool.manaFromHealth(entity.getMaxHealth());
         stdIncrease();
+    }
+
+    private void processManaDeficiency(ServerPlayerEntity player) {
+        var inventory = player.getInventory();
+        var effect = 0;
+
+        for (int i = 0; i < inventory.size(); i++) {
+            var stack = inventory.getStack(i);
+            var component = stack.get(ModComponents.ENTITY_STORAGE);
+            if (component != null && component.nbt().isPresent()) {
+                effect++;
+            }
+        }
+
+        // Handle the hat slot separately.
+        // Can we modify this to generically work with all accessories slots?
+        var hat = SlotReference.of(player, "hat", 0);
+        var component = hat.getStack().get(ModComponents.ENTITY_STORAGE);
+        if (component != null && component.nbt().isPresent()) {
+            effect++;
+        }
+
+        if (effect > 0) {
+            player.addStatusEffect(new StatusEffectInstance(
+                    ModEffects.MANA_DEFICIENCY,
+                    320, effect - 1,
+                    false, false, true
+            ));
+        }
+    }
+
+    @Override
+    public void stdIncrease() {
+        var multiplier = 1;
+
+        var hyperflux = entity.getStatusEffect(ModEffects.MANA_BOOST);
+        if (hyperflux != null) {
+            multiplier *= hyperflux.getAmplifier() + 2;
+        }
+
+        var hypoflux = entity.getStatusEffect(ModEffects.MANA_DEFICIENCY);
+        if (hypoflux != null) {
+            multiplier /= hypoflux.getAmplifier() + 2;
+        }
+
+        stdIncrease(multiplier);
     }
 
     @Override
--- src/main/java/dev/enjarai/trickster/effects/ManaDeficiencyEffect.java
+++ src/main/java/dev/enjarai/trickster/effects/ManaDeficiencyEffect.java
@@ -3,8 +3,8 @@ package dev.enjarai.trickster.effects;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 
-public class ManaBoostEffect extends StatusEffect {
-    protected ManaBoostEffect() {
-        super(StatusEffectCategory.BENEFICIAL, 0x42bfe8);
+public class ManaDeficiencyEffect extends StatusEffect {
+    protected ManaDeficiencyEffect() {
+        super(StatusEffectCategory.HARMFUL, 0xa67a61);
     }
 }
