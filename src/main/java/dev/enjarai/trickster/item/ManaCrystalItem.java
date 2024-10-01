fecaa157694c064136086b1533fd0a38b65d2843
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-10-01 17:35:12 -0400

    Update ManaCrystalItem.java

--- src/main/java/dev/enjarai/trickster/item/ManaCrystalItem.java
+++ src/main/java/dev/enjarai/trickster/item/ManaCrystalItem.java
@@ -10,5 +10,5 @@ public class ManaCrystalItem extends Item {
     // amethyst: one-time charge, no recharge
     // emerald: boring asf, mid capacity
     // diamond: also boring asf, tho high capacity
-    // echo shard: splits when ibuesddded
+    // echo shard: splits when ibuesddded (Aurora's hopefully clarifying comment: rai meant that the crafting recipe for a echo shard mana crystal would produce a pair which share the same mana pool)
 }
