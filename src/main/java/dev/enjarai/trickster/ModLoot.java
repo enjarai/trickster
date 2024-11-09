da144a0274c82af1f6bb83d2c8510dfb28e54667
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-11-09 15:56:51 -0500

    I love these silly things

--- src/main/java/dev/enjarai/trickster/ModLoot.java
+++ src/main/java/dev/enjarai/trickster/ModLoot.java
@@ -14,9 +14,16 @@ import net.minecraft.registry.RegistryKey;
 
 public class ModLoot {
     private static final Set<RegistryKey<LootTable>> RUSTED_SPELL_CORE_LOOT_TABLES = Set.of(
-            LootTables.SIMPLE_DUNGEON_CHEST, LootTables.ABANDONED_MINESHAFT_CHEST, LootTables.ANCIENT_CITY_CHEST,
-            LootTables.BASTION_TREASURE_CHEST, LootTables.DESERT_PYRAMID_CHEST, LootTables.JUNGLE_TEMPLE_CHEST,
-            LootTables.END_CITY_TREASURE_CHEST, LootTables.STRONGHOLD_CORRIDOR_CHEST, LootTables.STRONGHOLD_CROSSING_CHEST
+            LootTables.SIMPLE_DUNGEON_CHEST, LootTables.ABANDONED_MINESHAFT_CHEST,
+            LootTables.DESERT_PYRAMID_CHEST, LootTables.JUNGLE_TEMPLE_CHEST,
+            LootTables.UNDERWATER_RUIN_BIG_CHEST, LootTables.UNDERWATER_RUIN_SMALL_CHEST
+    );
+    private static final Set<RegistryKey<LootTable>> OMINOUS_SPELL_CORE_LOOT_TABLES = Set.of(
+            LootTables.TRIAL_CHAMBERS_REWARD_OMINOUS_RARE_CHEST
+    );
+    //TODO: this would be silly with merlin limit
+    private static final Set<RegistryKey<LootTable>> CRACKED_ECHO_KNOT_LOOT_TABLES = Set.of(
+            LootTables.ANCIENT_CITY_CHEST
     );
 
     public static void register() {
@@ -26,6 +33,13 @@ public class ModLoot {
                         .with(ItemEntry.builder(ModItems.RUSTED_SPELL_CORE)
                             .conditionally(RandomChanceLootCondition.builder(0.25f)))
                         .rolls(UniformLootNumberProvider.create(0, 2))).build();
+            }
+
+            if (OMINOUS_SPELL_CORE_LOOT_TABLES.contains(key)) {
+                tableBuilder.pool(LootPool.builder()
+                        .with(ItemEntry.builder(ModItems.OMINOUS_SPELL_CORE)
+                            .conditionally(RandomChanceLootCondition.builder(0.4f)))
+                        .rolls(UniformLootNumberProvider.create(0, 1))).build();
             }
         });
     }
