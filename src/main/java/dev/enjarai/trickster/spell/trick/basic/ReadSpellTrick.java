6e62d34f72829656a3be95b33f67bff6d4428fa5
cotton <jaiblueskyes@gmail.com>
2024-11-08 17:39:00 -0800

    added optional slot argument to ReadSpellTrick

--- src/main/java/dev/enjarai/trickster/spell/trick/basic/ReadSpellTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/basic/ReadSpellTrick.java
@@ -5,11 +5,17 @@ import dev.enjarai.trickster.item.component.ModComponents;
 import dev.enjarai.trickster.spell.Fragment;
 import dev.enjarai.trickster.spell.Pattern;
 import dev.enjarai.trickster.spell.SpellContext;
+import dev.enjarai.trickster.spell.fragment.FragmentType;
+import dev.enjarai.trickster.spell.fragment.SlotFragment;
 import dev.enjarai.trickster.spell.fragment.VoidFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.blunder.BlunderException;
+import net.minecraft.item.ItemStack;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ReadSpellTrick extends Trick {
     public ReadSpellTrick() {
@@ -18,7 +24,12 @@ public class ReadSpellTrick extends Trick {
 
     @Override
     public Fragment activate(SpellContext ctx, List<Fragment> fragments) throws BlunderException {
-        return ctx.source().getOtherHandStack(stack -> stack.contains(ModComponents.FRAGMENT))
+
+        Function<SlotFragment, ItemStack> getRef = slotFragment -> slotFragment.reference(this, ctx);
+        Optional<ItemStack> argument = supposeInput(fragments, FragmentType.SLOT, 0).map(getRef);
+        Supplier<Optional<ItemStack>> offhand = () -> ctx.source().getOtherHandStack(stack -> stack.contains(ModComponents.FRAGMENT));
+
+        return argument.or(offhand)
                 .map(stack -> stack.get(ModComponents.FRAGMENT))
                 .map(FragmentComponent::value)
                 .orElse(VoidFragment.INSTANCE);
