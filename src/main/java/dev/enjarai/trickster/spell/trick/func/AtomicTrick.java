845bb8216ddabc9d2a13b3b0843ba939177f3028
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-11-11 17:07:31 -0500

    I guess I missed something; shame

--- src/main/java/dev/enjarai/trickster/spell/trick/func/AtomicTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/func/AtomicTrick.java
@@ -24,6 +24,6 @@ public class AtomicTrick extends Trick implements ForkingTrick {
     @Override
     public SpellExecutor makeFork(SpellContext ctx, List<Fragment> fragments) throws BlunderException {
         var executable = expectInput(fragments, FragmentType.SPELL_PART, 0);
-        return new AtomicSpellExecutor(this, executable, ctx.state().recurseOrThrow(fragments.subList(1, fragments.size())));
+        return new AtomicSpellExecutor(this, ctx.data(), executable, ctx.state().recurseOrThrow(fragments.subList(1, fragments.size())));
     }
 }
