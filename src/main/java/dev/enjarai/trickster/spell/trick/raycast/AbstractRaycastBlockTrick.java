274d8045c5b68637dad8b2fbd35af928eff55365
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-11-18 13:40:12 -0300

    feat: Allow raycast to (optionally) find fluids
    and also "fix" stuff auri didn't bother changing/reading

--- src/main/java/dev/enjarai/trickster/spell/trick/raycast/AbstractRaycastBlockTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/raycast/AbstractRaycastBlockTrick.java
@@ -4,28 +4,36 @@ import dev.enjarai.trickster.spell.Fragment;
 import dev.enjarai.trickster.spell.Pattern;
 import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.blunder.BlunderException;
+import dev.enjarai.trickster.spell.fragment.BooleanFragment;
+import dev.enjarai.trickster.spell.fragment.FragmentType;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 
+import java.util.List;
 import java.util.Optional;
 
-public abstract class AbstractRaycastBlockTrick extends AbstractRaycastTrick {
+public abstract class AbstractRaycastBlockTrick extends AbstractRaycastTrick<Optional<BooleanFragment>> {
     public AbstractRaycastBlockTrick(Pattern pattern) {
         super(pattern);
     }
 
     @Override
-    public Fragment activate(SpellContext ctx, Optional<Entity> entity, Vec3d position, Vec3d direction) throws BlunderException {
+    protected Fragment extraContext(List<Fragment> fragments, SpellContext ctx, Optional<Entity> entity, Vec3d position, Vec3d direction) {
+        var fluids = supposeInput(fragments, FragmentType.BOOLEAN, 1);
+        return activate(ctx, entity, position, direction, fluids);
+    }
+
+    @Override
+    public Fragment activate(SpellContext ctx, Optional<Entity> entity, Vec3d position, Vec3d direction, Optional<BooleanFragment> extraContext) throws BlunderException {
+        boolean includeFluids = extraContext.orElse(BooleanFragment.FALSE).asBoolean();
         return activate(ctx.source().getWorld().raycast(new RaycastContext(position,
                 position.add(direction.multiply(64d)),
                 RaycastContext.ShapeType.OUTLINE,
-                RaycastContext.FluidHandling.NONE,
-                entity.isPresent()
-                        ? ShapeContext.of(entity.get())
-                        : ShapeContext.absent()
+                includeFluids ? RaycastContext.FluidHandling.ANY : RaycastContext.FluidHandling.NONE,
+                entity.map(ShapeContext::of).orElseGet(ShapeContext::absent)
         )));
     }
 
