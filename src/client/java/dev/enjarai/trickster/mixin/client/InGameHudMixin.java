cc2a67766a86d5bfda5e4d829085fe2372f25d97
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-12-15 19:55:45 -0500

    A gift

--- src/client/java/dev/enjarai/trickster/mixin/client/InGameHudMixin.java
+++ src/client/java/dev/enjarai/trickster/mixin/client/InGameHudMixin.java
@@ -12,7 +12,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,28 +20,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Inject(
-            method = "renderVignetteOverlay",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIFFIIII)V"
-            )
-    )
+    @Inject(method = "renderVignetteOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIFFIIII)V"))
     private void changeColorWhenFrozen(DrawContext context, Entity entity, CallbackInfo ci) {
-        if (entity instanceof LivingEntity livingEntity &&
-                livingEntity.getAttributes().hasModifierForAttribute(EntityAttributes.GENERIC_MOVEMENT_SPEED, Trickster.NEGATE_ATTRIBUTE.id())) {
+        if (entity instanceof LivingEntity livingEntity
+                && livingEntity.getAttributes().hasModifierForAttribute(EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                        Trickster.NEGATE_ATTRIBUTE.id())) {
             context.setShaderColor(0.4f, 0.4f, 0f, 1f);
         }
     }
 
-    @Inject(
-            method = "renderHotbar",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/option/SimpleOption;getValue()Ljava/lang/Object;"
-            )
-    )
-    private void renderHatHud(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci, @Local PlayerEntity player) {
+    @Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption;getValue()Ljava/lang/Object;"))
+    private void renderHatHud(DrawContext context,
+            RenderTickCounter tickCounter,
+            CallbackInfo ci,
+            @Local PlayerEntity player) {
         var hatStack = player.getOffHandStack();
         if (hatStack.isIn(ModItems.HOLDABLE_HAT)) {
             var matrices = context.getMatrices();
@@ -55,16 +46,19 @@ public class InGameHudMixin {
 
             for (int i = 0; i < 3; i++) {
                 var offset = i - 1;
-                matrices.push();
-                matrices.translate(0, 0, -Math.abs(offset));
-
                 var scrollStack = TrickHatItem.getScrollRelative(hatStack, offset);
-                var brightness = offset == 0 ? 1f : 0.6f;
-                RenderSystem.setShaderColor(brightness, brightness, brightness, brightness);
 
-                context.drawItem(scrollStack, x + offset * 8, y);
+                if (!scrollStack.equals(TrickHatItem.getScrollRelative(hatStack, 0))) {
+                    matrices.push();
+                    matrices.translate(0, 0, -Math.abs(offset));
 
-                matrices.pop();
+                    var brightness = offset == 0 ? 1f : 0.6f;
+                    RenderSystem.setShaderColor(brightness, brightness, brightness, brightness);
+
+                    context.drawItem(scrollStack, x + offset * 8, y);
+
+                    matrices.pop();
+                }
             }
 
             RenderSystem.setShaderColor(1, 1, 1, 1);
