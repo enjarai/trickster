39eb1e2b284043d06bb4f4bfdaee48eff9bb9f11
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2025-01-08 01:14:05 -0500

    Oy, coming along. I'll regret this tomorrow...

--- src/main/java/dev/enjarai/trickster/spell/trick/misc/DelayExecutionTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/misc/DelayExecutionTrick.java
@@ -6,18 +6,18 @@ import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.fragment.FragmentType;
 import dev.enjarai.trickster.spell.fragment.NumberFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
+import dev.enjarai.trickster.spell.type.Signature;
 import dev.enjarai.trickster.spell.blunder.BlunderException;
 
-import java.util.List;
+import java.util.Optional;
 
-public class DelayExecutionTrick extends Trick {
+public class DelayExecutionTrick extends Trick<DelayExecutionTrick> {
     public DelayExecutionTrick() {
-        super(Pattern.of(0, 2, 4, 6, 8, 4, 0));
+        super(Pattern.of(0, 2, 4, 6, 8, 4, 0), Signature.of(FragmentType.NUMBER.optionalOf(), DelayExecutionTrick::run));
     }
 
-    @Override
-    public Fragment activate(SpellContext ctx, List<Fragment> fragments) throws BlunderException {
-        var amount = supposeInput(fragments, FragmentType.NUMBER, 0).map(n -> Math.round(n.asInt())).orElse(1);
+    public Fragment run(SpellContext ctx, Optional<NumberFragment> number) throws BlunderException {
+        var amount = number.map(n -> Math.round(n.asInt())).orElse(1);
         ctx.state().addDelay(amount);
         return new NumberFragment(amount);
     }
