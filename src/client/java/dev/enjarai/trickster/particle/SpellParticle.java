428208476218c287caf34a5bd67c9940ecfbfc1f
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2025-01-08 01:37:57 -0500

    Aaaaaa

--- src/client/java/dev/enjarai/trickster/particle/SpellParticle.java
+++ src/client/java/dev/enjarai/trickster/particle/SpellParticle.java
@@ -3,22 +3,15 @@ package dev.enjarai.trickster.particle;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.*;
-import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.util.math.ColorHelper;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
 public class SpellParticle extends SpriteBillboardParticle {
     protected SpellParticle(ClientWorld world, double x, double y, double z, double dx, double dy, double dz, int color) {
         super(world, x, y, z);
-//        maxAge = 10;
-//        alpha = 0.6f;
+        //        maxAge = 10;
+        //        alpha = 0.6f;
         red = ColorHelper.Argb.getRed(color) / 255f;
         green = ColorHelper.Argb.getGreen(color) / 255f;
         blue = ColorHelper.Argb.getBlue(color) / 255f;
