0da8e1278ad61ea8b001443bcbcd90ecfe8f0f90
Aurora Dawn <aurora.dawn@trans.fish>
2025-01-21 14:13:28 -0500

    owo?

---
+++ src/main/java/dev/enjarai/trickster/util/Unit.java
@@ -0,0 +1,119 @@
+package dev.enjarai.trickster.util;
+
+public abstract class Unit {
+    protected final int precision;
+
+    public Unit(int precision) {
+        this.precision = precision;
+
+        if (precision < -1 || precision > 4) {
+            throw new IllegalStateException("Unit must have a precision in the range of -1 and 4 but got " + precision);
+        }
+    }
+
+    public float correct(float value) {
+        return (float) (value / Math.pow(1000, precision - 1));
+    }
+
+    public String shortName() {
+        return shortNameStart() + shortNameEnd();
+    }
+
+    public String longName() {
+        return longNameStart() + longNameEnd();
+    }
+
+    private String shortNameStart() {
+        switch (precision) {
+            case 4:
+                return "T";
+            case 3:
+                return "G";
+            case 2:
+                return "M";
+            case 1:
+                return "k";
+            case 0:
+                return "";
+            case -1:
+                return "m";
+            default:
+                throw new IllegalStateException("Unit must have a precision in the range of -1 and 4 but got " + precision);
+        }
+    }
+
+    private String longNameStart() {
+        switch (precision) {
+            case 4:
+                return "tera";
+            case 3:
+                return "giga";
+            case 2:
+                return "mega";
+            case 1:
+                return "kilo";
+            case 0:
+                return "";
+            case -1:
+                return "milli";
+            default:
+                throw new IllegalStateException("Unit must have a precision in the range of -1 and 4 but got " + precision);
+        }
+    }
+
+    protected abstract String shortNameEnd();
+
+    protected abstract String longNameEnd();
+
+    public static Gandalf getGandalfUnit(float value) {
+        return new Gandalf(kiloUsageToPrecision(value));
+    }
+
+    public static Merlin getMerlinUnit(float value) {
+        return new Merlin(kiloUsageToPrecision(value));
+    }
+
+    private static int kiloUsageToPrecision(float value) {
+        int precision = 4;
+        double d = Math.abs(value) / Math.pow(1000, 3);
+
+        while (precision > -1 && d < 10) {
+            precision -= 1;
+            d *= 1000;
+        }
+
+        return precision;
+    }
+
+    public static class Gandalf extends Unit {
+        public Gandalf(int precision) {
+            super(precision);
+        }
+
+        @Override
+        protected String shortNameEnd() {
+            return "G";
+        }
+
+        @Override
+        protected String longNameEnd() {
+            return "Gandalf";
+        }
+    }
+
+    public static class Merlin extends Unit {
+        public Merlin(int precision) {
+            super(precision);
+        }
+
+        @Override
+        protected String shortNameEnd() {
+            return "M";
+        }
+
+        @Override
+        protected String longNameEnd() {
+            return "Merlin";
+        }
+    }
+}
