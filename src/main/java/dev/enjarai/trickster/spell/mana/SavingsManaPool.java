07da726964a36e07d00366625f72923ee6717039
Aurora Dawn <aurora.dawn@trans.fish>
2025-01-21 10:35:45 -0500

    Redundant clamp removed; structure remains stable

--- src/main/java/dev/enjarai/trickster/spell/mana/SavingsManaPool.java
+++ src/main/java/dev/enjarai/trickster/spell/mana/SavingsManaPool.java
@@ -47,7 +47,7 @@ public class SavingsManaPool extends SimpleManaPool {
         var lastMana = super.get(world); // P
 
         // P(1 + i)^t
-        set((float) Math.clamp(lastMana * Math.pow(1 + interest, ticksPassed), 0, getMax(world)), world);
+        set((float) (lastMana * Math.pow(1 + interest, ticksPassed)), world);
         return super.get(world);
     }
 
