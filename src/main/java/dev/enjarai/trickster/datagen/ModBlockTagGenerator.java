94f28265748a52e37ead8ed8f55eccfb14fbd10d
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2025-01-22 19:17:33 -0300

    rebase

---
+++ src/main/java/dev/enjarai/trickster/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,21 @@
+package dev.enjarai.trickster.datagen;
+
+import dev.enjarai.trickster.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends FabricTagProvider.BlockTagProvider {
+    public ModBlockTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(BlockTags.REPLACEABLE)
+          .add(ModBlocks.LIGHT);
+    }
+}
