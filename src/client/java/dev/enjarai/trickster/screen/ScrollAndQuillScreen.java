bbeba6ea515c33b1dfc27d2b6a9b06e7a570a60d
unlessgames <unlessgames@gmail.com>
2025-03-25 05:55:16 +0100

    add editor exit with inventory key, navigation with movement keys

--- src/client/java/dev/enjarai/trickster/screen/ScrollAndQuillScreen.java
+++ src/client/java/dev/enjarai/trickster/screen/ScrollAndQuillScreen.java
@@ -2,8 +2,10 @@ package dev.enjarai.trickster.screen;
 
 import dev.enjarai.trickster.item.ModItems;
 import dev.enjarai.trickster.spell.SpellPart;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 
@@ -17,6 +19,15 @@ public class ScrollAndQuillScreen extends Screen implements ScreenHandlerProvide
     public SpellPartWidget partWidget;
 
     private boolean hasLoaded = false;
+
+    static final double ZOOM_SPEED = 1.0;
+    static final double PAN_SPEED = 9.0;
+    static final double SPRINT_MULTIPLIER = 1.4142;
+
+    private double inputX = 0;
+    private double inputY = 0;
+    private double inputZ = 0;
+    private boolean inputSprint = false;
 
     public ScrollAndQuillScreen(ScrollAndQuillScreenHandler handler, PlayerInventory playerInventory, Text title) {
         super(title);
@@ -88,6 +99,76 @@ public class ScrollAndQuillScreen extends Screen implements ScreenHandlerProvide
     public boolean mouseReleased(double mouseX, double mouseY, int button) {
         if (this.isDragging()) this.setDragging(false);
         return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        
+        double deltaWithSprint = inputSprint ? SPRINT_MULTIPLIER * delta: delta;
+        if (inputZ != 0.0) {
+            partWidget.mouseScrolled(mouseX, mouseY, 0.0, inputZ * ZOOM_SPEED * deltaWithSprint);
+        }
+        if (inputX != 0.0 || inputY != 0.0) {
+            partWidget.mouseDragged(mouseX, mouseY, 0, inputX * PAN_SPEED * deltaWithSprint, inputY * PAN_SPEED * deltaWithSprint);
+        }
+    }
+   
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (this.client.options.inventoryKey.matchesKey(keyCode, scanCode)) {
+            this.close();
+            return true;
+        } else if (this.client.options.forwardKey.matchesKey(keyCode, scanCode)) {
+            inputZ = 1.0;
+            return true;
+        } else if (this.client.options.backKey.matchesKey(keyCode, scanCode)) {
+            inputZ = -1.0;
+            return true;
+        } else if (this.client.options.leftKey.matchesKey(keyCode, scanCode)) {
+            inputX = 1.0;
+            return true;
+        } else if (this.client.options.rightKey.matchesKey(keyCode, scanCode)) {
+            inputX = -1.0;
+            return true;
+        } else if (this.client.options.sneakKey.matchesKey(keyCode, scanCode)) {
+            inputY = -1.0;
+            return true;
+        } else if (this.client.options.jumpKey.matchesKey(keyCode, scanCode)) {
+            inputY = 1.0;
+            return true;
+        } else if (this.client.options.sprintKey.matchesKey(keyCode, scanCode)) {
+            inputSprint = true;
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+    
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        if (this.client.options.forwardKey.matchesKey(keyCode, scanCode)) {
+            inputZ = 0.0;
+            return true;
+        } else if (this.client.options.backKey.matchesKey(keyCode, scanCode)) {
+            inputZ = 0.0;
+            return true;
+        } else if (this.client.options.leftKey.matchesKey(keyCode, scanCode)) {
+            inputX = 0.0;
+            return true;
+        } else if (this.client.options.rightKey.matchesKey(keyCode, scanCode)) {
+            inputX = 0.0;
+            return true;
+        } else if (this.client.options.sneakKey.matchesKey(keyCode, scanCode)) {
+            inputY = 0.0;
+            return true;
+        } else if (this.client.options.jumpKey.matchesKey(keyCode, scanCode)) {
+            inputY = 0.0;
+            return true;
+        } else if (this.client.options.sprintKey.matchesKey(keyCode, scanCode)) {
+            inputSprint = false;
+            return true;
+        }
+        return super.keyReleased(keyCode, scanCode, modifiers);
     }
 
     @Override
