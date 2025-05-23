691ca4b0416b74bc895f9a38278d52c07e0d03ce
enjarai <enjarai@trans.fish>
2025-05-23 23:55:38 +0200

    Useless cat

--- src/main/java/dev/enjarai/trickster/datagen/StateToManaConversionGenerator.java
+++ src/main/java/dev/enjarai/trickster/datagen/StateToManaConversionGenerator.java
@@ -23,6 +23,8 @@ public class StateToManaConversionGenerator extends StateToManaConversionProvide
         //region Tags
         createConversion(BlockTags.LEAVES).add(7.12f); // this is for the sake of Evelyn's suffering
         createConversion(BlockTags.CAVE_VINES).add(16, new Value<>(Properties.BERRIES, true)).add(4);
+        createConversion(BlockTags.SMALL_FLOWERS).add(4);
+        createConversion(BlockTags.TALL_FLOWERS).add(8);
 
         //region Misc
         copyOrCreateConversion(Blocks.MELON).add(24);
@@ -31,15 +33,30 @@ public class StateToManaConversionGenerator extends StateToManaConversionProvide
         copyOrCreateConversion(Blocks.CARVED_PUMPKIN).add(16);
         copyOrCreateConversion(Blocks.PUMPKIN_STEM).add(4);
         copyOrCreateConversion(Blocks.BAMBOO).add(2);
+        copyOrCreateConversion(Blocks.BAMBOO_SAPLING).add(0);
         copyOrCreateConversion(Blocks.SUGAR_CANE).add(12);
         copyOrCreateConversion(Blocks.CACTUS).add(24);
         copyOrCreateConversion(Blocks.KELP).add(2);
         copyOrCreateConversion(Blocks.KELP_PLANT).add(2); //TODO: fix waterlogged losing water
         copyOrCreateConversion(Blocks.SWEET_BERRY_BUSH)
-                .add(0, new Value<>(Properties.AGE_3, 0))
-                .add(4, new Value<>(Properties.AGE_3, 1))
-                .add(8, new Value<>(Properties.AGE_3, 2))
-                .add(16, new Value<>(Properties.AGE_3, 3));
+            .add(0, new Value<>(Properties.AGE_3, 0))
+            .add(4, new Value<>(Properties.AGE_3, 1))
+            .add(12, new Value<>(Properties.AGE_3, 2))
+            .add(16, new Value<>(Properties.AGE_3, 3));
+        copyOrCreateConversion(Blocks.BROWN_MUSHROOM).add(8);
+        copyOrCreateConversion(Blocks.RED_MUSHROOM).add(8);
+        copyOrCreateConversion(Blocks.BROWN_MUSHROOM_BLOCK).add(24);
+        copyOrCreateConversion(Blocks.RED_MUSHROOM_BLOCK).add(24);
+        copyOrCreateConversion(Blocks.MUSHROOM_STEM).add(24);
+        copyOrCreateConversion(Blocks.MOSS_BLOCK).add(8);
+        copyOrCreateConversion(Blocks.MOSS_CARPET).add(1);
+        copyOrCreateConversion(Blocks.AZALEA).add(6);
+        copyOrCreateConversion(Blocks.FLOWERING_AZALEA).add(6.1f); // flowers are tasty :3
+        copyOrCreateConversion(Blocks.SHORT_GRASS).add(2);
+        copyOrCreateConversion(Blocks.TALL_GRASS).add(2);
+        copyOrCreateConversion(Blocks.SEAGRASS).add(2);
+        copyOrCreateConversion(Blocks.TALL_SEAGRASS).add(2);
+        copyOrCreateConversion(Blocks. awawawawa)
 
         //region Crops
         configureCrop(Blocks.CARROTS);
