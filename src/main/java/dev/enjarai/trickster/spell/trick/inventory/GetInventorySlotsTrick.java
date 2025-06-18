7b76364e20b05bc8c867e3b4bcfc34adb1b82063
Nicholas Kinney <kinneyanick@gmail.com>
2025-06-18 12:17:21 -0500

    Added variadic item type filtering behavior

--- src/main/java/dev/enjarai/trickster/spell/trick/inventory/GetInventorySlotsTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/inventory/GetInventorySlotsTrick.java
@@ -7,31 +7,48 @@ import dev.enjarai.trickster.spell.blunder.BlunderException;
 import dev.enjarai.trickster.spell.blunder.UnknownEntityBlunder;
 import dev.enjarai.trickster.spell.fragment.EntityFragment;
 import dev.enjarai.trickster.spell.fragment.FragmentType;
+import dev.enjarai.trickster.spell.fragment.ItemTypeFragment;
+import dev.enjarai.trickster.spell.fragment.ListFragment;
 import dev.enjarai.trickster.spell.fragment.SlotFragment;
 import dev.enjarai.trickster.spell.fragment.VectorFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.type.Signature;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.mojang.datafixers.util.Either;
 
 public class GetInventorySlotsTrick extends Trick<GetInventorySlotsTrick> {
     public GetInventorySlotsTrick() {
-        super(Pattern.of(5, 8), Signature.of(GetInventorySlotsTrick::fromCaster));
-        overload(Signature.of(FragmentType.VECTOR, GetInventorySlotsTrick::fromVector));
-        overload(Signature.of(FragmentType.ENTITY, GetInventorySlotsTrick::fromEntity));
+        super(Pattern.of(7, 4, 3, 0, 2, 5, 4, 0, 7, 2, 4), Signature.of(variadic(FragmentType.ITEM_TYPE).unpack(), GetInventorySlotsTrick::fromCaster));
+        overload(Signature.of(FragmentType.VECTOR, variadic(FragmentType.ITEM_TYPE).unpack(), GetInventorySlotsTrick::fromVector));
+        overload(Signature.of(FragmentType.ENTITY, variadic(FragmentType.ITEM_TYPE).unpack(), GetInventorySlotsTrick::fromEntity));
     }
 
-    public Fragment fromCaster(SpellContext ctx) throws BlunderException {
-        return SlotFragment.getSlots(this, ctx, Optional.empty());
+    private Fragment fromSlots(SpellContext ctx, ListFragment slots, List<ItemTypeFragment> itemTypes) {
+        var itemTypesFilter = itemTypes.stream().map(typeFragment -> {
+            return typeFragment.item();
+        }).toList();
+
+        if (itemTypes.size() > 0) {
+            return new ListFragment(slots.fragments().stream().filter(slot -> (slot instanceof SlotFragment slotFragment) && itemTypesFilter.contains(slotFragment.getItem(this, ctx))
+            ).toList());
+        } else {
+            return slots;
+        }
     }
 
-    public Fragment fromVector(SpellContext ctx, VectorFragment pos) throws BlunderException {
-        return SlotFragment.getSlots(this, ctx, Optional.of(Either.left(pos.toBlockPos())));
+    public Fragment fromCaster(SpellContext ctx, List<ItemTypeFragment> itemTypes) throws BlunderException {
+        var slots = SlotFragment.getSlots(this, ctx, Optional.empty());
+        return fromSlots(ctx, slots, itemTypes);
     }
 
-    public Fragment fromEntity(SpellContext ctx, EntityFragment entity) throws BlunderException {
-        return SlotFragment.getSlots(this, ctx, Optional.of(Either.right(entity.getEntity(ctx).orElseThrow(() -> new UnknownEntityBlunder(this)).getUuid())));
+    public Fragment fromVector(SpellContext ctx, VectorFragment pos, List<ItemTypeFragment> itemTypes) throws BlunderException {
+        return fromSlots(ctx, SlotFragment.getSlots(this, ctx, Optional.of(Either.left(pos.toBlockPos()))), itemTypes);
+    }
+
+    public Fragment fromEntity(SpellContext ctx, EntityFragment entity, List<ItemTypeFragment> itemTypes) throws BlunderException {
+        return fromSlots(ctx, SlotFragment.getSlots(this, ctx, Optional.of(Either.right(entity.getEntity(ctx).orElseThrow(() -> new UnknownEntityBlunder(this)).getUuid()))), itemTypes);
     }
 }
