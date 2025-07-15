9e4fde542b5a10eccb39f0e6ecf591d82207860e
Nicholas Kinney <kinneyanick@gmail.com>
2025-07-15 18:24:37 -0500

    Changes as requested by Auri

--- src/main/java/dev/enjarai/trickster/spell/trick/func/GetSpellFromSlotTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/func/GetSpellFromSlotTrick.java
@@ -1,6 +1,8 @@
 
 package dev.enjarai.trickster.spell.trick.func;
 
+import java.util.Optional;
+
 import dev.enjarai.trickster.spell.Pattern;
 import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.SpellPart;
@@ -14,11 +16,12 @@ import dev.enjarai.trickster.spell.type.Signature;
 public class GetSpellFromSlotTrick extends Trick<GetSpellFromSlotTrick> {
     public GetSpellFromSlotTrick() {
         super(Pattern.of(5, 4, 3, 0, 1, 4, 7, 6, 3),
-                Signature.of(FragmentType.NUMBER, GetSpellFromSlotTrick::run, FragmentType.SPELL_PART));
+                Signature.of(FragmentType.NUMBER.optionalOfArg(), GetSpellFromSlotTrick::run, FragmentType.SPELL_PART.optionalOfRet()));
     }
 
-    public SpellPart run(SpellContext ctx, NumberFragment spellSlot) throws BlunderException {
+    public Optional<SpellPart> run(SpellContext ctx, Optional<NumberFragment> maybeSpellSlot) throws BlunderException {
+        var spellSlot = maybeSpellSlot.orElse(new NumberFragment(ctx.data().getSlot().orElseThrow(() -> new IncompatibleSourceBlunder(this))));
         var manager = ctx.source().getExecutionManager().orElseThrow(() -> new IncompatibleSourceBlunder(this));
-        return manager.getSpell(spellSlot.asInt()).orElse(new SpellPart());
+        return manager.getSpell(spellSlot.asInt());
     }
 }
