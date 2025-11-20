66c8d931d25316323b66b8e9b74b0087f6610d11
AshAxolotl <87580179+AshAxolotl@users.noreply.github.com>
2025-11-20 19:37:58 +0100

    reveal changes + config removal

--- src/main/java/dev/enjarai/trickster/spell/trick/basic/RevealTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/basic/RevealTrick.java
@@ -1,11 +1,10 @@
 package dev.enjarai.trickster.spell.trick.basic;
 
-import dev.enjarai.trickster.Trickster;
-import dev.enjarai.trickster.net.ModNetworking;
 import dev.enjarai.trickster.spell.Fragment;
 import dev.enjarai.trickster.spell.Pattern;
 import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.blunder.BlunderException;
+import dev.enjarai.trickster.spell.blunder.NoPlayerBlunder;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.type.ArgType;
 import dev.enjarai.trickster.spell.type.RetType;
@@ -38,9 +37,7 @@ public class RevealTrick extends Trick<RevealTrick> {
         }
 
         Text finalResult = result;
-        ctx.source().getPlayer().ifPresent(player -> {
-            player.sendMessage(Text.of(finalResult), ModNetworking.clientOrDefault(player, Trickster.CONFIG.keys.revealToHotbar, Trickster.CONFIG.revealToHotbar()));
-        });
+        ctx.source().getPlayer().orElseThrow(() -> new NoPlayerBlunder(this)).sendMessage(Text.of(finalResult), false);
 
         return first;
     }
