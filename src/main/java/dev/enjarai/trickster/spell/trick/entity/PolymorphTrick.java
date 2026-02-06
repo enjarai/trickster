a3146209a6661705660160238b6cfa94d2dab256
AshAxolotl <git@ashaxolotl.com>
2026-02-06 19:27:11 +0100

    fix reverse inputs + returns target

--- src/main/java/dev/enjarai/trickster/spell/trick/entity/PolymorphTrick.java
+++ src/main/java/dev/enjarai/trickster/spell/trick/entity/PolymorphTrick.java
@@ -6,22 +6,21 @@ import dev.enjarai.trickster.spell.SpellContext;
 import dev.enjarai.trickster.spell.blunder.UnknownEntityBlunder;
 import dev.enjarai.trickster.spell.fragment.EntityFragment;
 import dev.enjarai.trickster.spell.fragment.FragmentType;
-import dev.enjarai.trickster.spell.fragment.VoidFragment;
 import dev.enjarai.trickster.spell.trick.Trick;
 import dev.enjarai.trickster.spell.type.Signature;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class PolymorphTrick extends Trick<PolymorphTrick> {
     public PolymorphTrick() {
-        super(Pattern.of(4, 2, 1, 0, 4, 8, 7, 6, 4), Signature.of(FragmentType.ENTITY.wardOf(), FragmentType.ENTITY, PolymorphTrick::morph, FragmentType.VOID));
+        super(Pattern.of(4, 2, 1, 0, 4, 8, 7, 6, 4), Signature.of(FragmentType.ENTITY.wardOf(), FragmentType.ENTITY, PolymorphTrick::morph, FragmentType.ENTITY));
     }
 
-    public VoidFragment morph(SpellContext ctx, EntityFragment source, EntityFragment target) {
+    public EntityFragment morph(SpellContext ctx, EntityFragment target, EntityFragment source) {
         var realSource = source.getEntity(ctx).orElseThrow(() -> new UnknownEntityBlunder(this));
         var realTarget = target.getEntity(ctx).orElseThrow(() -> new UnknownEntityBlunder(this));
 
         if (realSource.getUuid().equals(realTarget.getUuid()))
-            return VoidFragment.INSTANCE;
+            return target;
 
         if (realTarget instanceof ServerPlayerEntity targetPlayer && realSource instanceof ServerPlayerEntity sourcePlayer) {
             ctx.useMana(this, 8000);
@@ -39,6 +38,6 @@ public class PolymorphTrick extends Trick<PolymorphTrick> {
             throw new UnknownEntityBlunder(this);
         }
 
-        return VoidFragment.INSTANCE;
+        return target;
     }
 }
